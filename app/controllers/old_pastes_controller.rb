50be1743eb568312978b4cb75356c9b7308a884b
David Southard <nacengineer@gmail.com>
2012-12-17 21:01:01 -0600

    Move old pastes to a separate controller

---
+++ app/controllers/old_pastes_controller.rb
@@ -0,0 +1,70 @@
+class OldPastesController < ApplicationController
+  def index
+    @pastes = Paste.expired.paginate(:per_page => 10, :page => params[:page]).order('created_at DESC')
+    respond_to do |format|
+      format.html { render :index } # index.html.erb
+      format.json { render json: @pastes }
+    end
+  end
+
+  def show
+    @paste = Paste.find_by_id(params[:id])
+
+    respond_to do |format|
+      format.html # show.html.erb
+      format.json { render json: @paste }
+    end
+  end
+
+  def new
+    @paste = Paste.new
+
+    respond_to do |format|
+      format.html # new.html.erb
+      # format.json { render json: @paste }
+    end
+  end
+
+  def edit
+    @paste = Paste.find_by_id(params[:id])
+  end
+
+  def create
+    @paste = Paste.new(params[:paste])
+
+    respond_to do |format|
+      if @paste.save
+        format.html { redirect_to @paste, notice: 'Paste was successfully created.' }
+        # format.json { render json: @paste, status: :created, location: @paste }
+      else
+        format.html { render action: "new" }
+        # format.json { render json: @paste.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def update
+    @paste = Paste.find_by_id(params[:id])
+
+    respond_to do |format|
+      if @paste.update_attributes(params[:paste])
+        format.html { redirect_to @paste, notice: 'Paste was successfully updated.' }
+        # format.json { head :ok }
+      else
+        format.html { render action: "edit" }
+        # format.json { render json: @paste.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def destroy
+    @paste = Paste.find_by_id(params[:id])
+    @paste.destroy
+
+    respond_to do |format|
+      format.html { redirect_to pastes_url }
+      # format.json { head :ok }
+    end
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -49,13 +49,9 @@ RorPaste::Application.routes.draw do
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
 
-  get 'help/markdown'     => 'help#markdown'
+  get 'help/markdown' => 'help#markdown'
 
-  resources :pastes do
-    collection do
-      get 'expired',     :to => "pastes#expired"
-    end
-  end
+  resources :pastes, :old_pastes
 
   root :to => 'pastes#new'
 
