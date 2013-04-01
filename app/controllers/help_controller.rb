6a085f044840508f07c537e5ff42bf9e0e741ac9
David Southard <nacengineer@gmail.com>
2013-04-01 12:37:21 -0500

    refactor old pastes, help controllers

--- app/controllers/help_controller.rb
+++ app/controllers/help_controller.rb
@@ -1,4 +1,2 @@
 class HelpController < ApplicationController
-
-
 end
--- app/controllers/old_pastes_controller.rb
+++ app/controllers/old_pastes_controller.rb
@@ -1,6 +1,6 @@
 class OldPastesController < ApplicationController
   def index
-    @pastes = Paste.expired.paginate(:per_page => 10, :page => params[:page]).order('created_at DESC')
+    @pastes = Paste.expired.paginate(page: params[:page])
     respond_to do |format|
       format.html { render :index } # index.html.erb
       format.json { render json: @pastes }
